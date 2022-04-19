43bd3da5419622755cadf9ae2f427f4d18fb9940
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-04-18 18:50:34 -0700

    move some files that were in the wrong place and update imports,

--- dao-frontend/src/index.js
+++ dao-frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Wrapper from './Wrapper';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Wrapper/>
   </React.StrictMode>
 );
 
