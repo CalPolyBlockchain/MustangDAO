1bf7a4eb81780407caf89d8ea035ab1c882f3559
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-04-27 16:33:56 -0700

    rough update for home

--- dao-frontend/src/pages/Home.js
+++ dao-frontend/src/pages/Home.js
@@ -2,7 +2,10 @@
 
 export default function Home(props) {
     return (
-        <div className="container">
-            <p></p>
+        <div className="items-center bg-darkBlue1 p-5">
+            <p className="text-3xl text-white text-center p-3 m-3"> Welcome to MustangDao</p>
+            <div className="aspect-square rounded-full bg-white bg-opacity-30 p-10">
+            <img className="w-80 h-auto pr-1" src="./stang.png"></img>
+            </div>
         </div>);
 }
