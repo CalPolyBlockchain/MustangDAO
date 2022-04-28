0cb2fed287dbafa7622c651c4eb80fb2e7d5959f
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-04-28 01:26:23 -0700

    Co-authored-by: Max Mohammadi <MaxMohammadi@users.noreply.github.com> Co-authored-by: Vaibhav Garg <vboo123@users.noreply.github.com>

--- dao-frontend/src/App.js
+++ dao-frontend/src/App.js
@@ -6,16 +6,32 @@ import ProposalView from './components/ProposalView';
 const user = {
   name: "John Doe",
   balance: "100",
-  vote_count: "10"
+  vote_count: "10",
+  proposal_count: "4",
+  active_proposals: "2",
+  emoji: "👾",
+  roles: ["dev", "designer"]
 }
+
+const prop_tags = [
+  {name:"Gov.", description:"Governance", color:"blue"},
+  {name:"Funds", description:"allocation of funds", color:"blue"},
+  {name:"Meeting", description:"orchestration of meeting ", color:"yellow2"},
+  {name:"Project", description:"Project", color:"darkBlue2"},
+  {name:"Snack", description:"Governance", color:"green2"},
+
+]
+
 
 const proposals = [
   {
     id: 1,
     name: "Proposal 1",
     description: "This is proposal 1",
+    tags : [prop_tags[0]],
     status: "Open",
     vote_count: "10",
+    quorum: "15",
     votes: [{ "option": "Yes", "count": 10 },
     { "option": "No", "count": 4 }]
   },
@@ -23,8 +39,10 @@ const proposals = [
     id: 1,
     name: "Proposal 1",
     description: "This is proposal 1",
+    tags : [prop_tags[0]],
     status: "Open",
     vote_count: "10",
+    quorum: "15",
     votes: [{ "option": "Yes", "count": 10 },
     { "option": "No", "count": 4 }]
   },
@@ -32,15 +50,17 @@ const proposals = [
     id: 1,
     name: "Proposal 1",
     description: "This is proposal 1",
+    tags : [prop_tags[0]],
     status: "Open",
     vote_count: "10",
+    quorum: "15",
     votes: [{ "option": "Yes", "count": 10 },
     { "option": "No", "count": 4 }]
   },
 ]
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <body>
         {/* ~ ROUGH outline of page  order listed does not matter*/}
         <UserHeader {...user} />
@@ -48,6 +68,7 @@ function App() {
         {/* SHOW active proposals (User should be able to click and vote on these) */}
         {/* Let user Create Proposal (modal components) */}
         <ProposalView proposals={proposals} />
+
 
         {/*CONNECT User wallet*/}
 
