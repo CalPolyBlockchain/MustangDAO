47129c72e3e0dda6a0d3d755f97850c925f2cf7a
Evan W <ewitulsk@calpoly.edu>
2022-04-27 17:33:20 -0700

    inital hardhat commit

---
+++ contracts/contracts/STANG.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+
+contract STANG is ERC20, ERC20Snapshot {
+
+    constructor() ERC20("MustangDao", "STANG") {
+        
+    }
+
+    function snapshot() public returns(uint256){
+        return(_snapshot());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
